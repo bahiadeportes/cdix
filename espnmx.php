d96faa37805d103f974ad88f63c515e54f892b7a
bahiadeportes <117000631+bahiadeportes@users.noreply.github.com>
2023-02-18 17:35:28 -0600

    Create espnmx.php

---
+++ espnmx.php
@@ -0,0 +1,64 @@
+
+<html>
+	<head>
+		
+		<title>ESPN MEXICO Opcion 2</title>
+<meta content='width=device-width, initial-scale=1' name='viewport'>	
+<meta http-equiv="Content-Language" content="es" />	
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">	
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z37PD0SFFX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z37PD0SFFX');
+</script>
+
+	</head>
+
+<body  style="padding: 0px; margin:0px;">
+<script src="//ssl.p.jwpcdn.com/player/v/8.21.0/jwplayer.js"></script>
+<script> jwplayer.key = 'XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo';
+</script>
+<div id="player"></div>
+<script>
+
+    var playerInstance = jwplayer("player");
+
+    playerInstance.setup({
+
+        playlist: [{
+            "title": "REPRODUCTOR",
+            "sources": [
+                {
+                    "default": false,
+                    "type": "dash",
+                    "file": '//1108-vos.dtvott.com/DASH/manifest.mpd',
+                    "drm": {
+                        "clearkey": { "keyId": "591cdbc97034330b849614a6efbfdfd4", "key": "d664407707d260f93908121d84df4eba" }
+                    },
+                    "label": "0"
+                }
+            ]
+        }],
+        width: "100%",
+        height: "100%",
+        aspectratio: "16:9",
+autostart: true,
+			 logo: {
+ 					 file: '',
+ 					 link: 'https://bahiadeportes.com/',
+  						position: 'top-right'
+  					},
+			cast:{},
+			sharing: {}
+    });
+</script>
+ <script src="//ahscdn.com/script/suv4.js" data-adel="lwsu" cdnd="ahscdn.com" zid="5290947"></script>
+
+
+<body>
+
+</html>
